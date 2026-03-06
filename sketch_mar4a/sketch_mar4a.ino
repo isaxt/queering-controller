3bd7a299e028113f808d68c18bd0d55498ba29ee
Ruoyu50 <sssssmess@foxmail.com>
2026-03-06 14:57:05 -0500

    update connected code_5sensors

--- sketch_mar4a/sketch_mar4a.ino
+++ sketch_mar4a/sketch_mar4a.ino
@@ -2,6 +2,7 @@
 #define FSR_1 A1
 #define FSR_2 A2
 #define FSR_3 A3
+#define FSR_4 A4
 
 void setup() {
   Serial.begin(9600);
@@ -13,14 +14,21 @@ void loop() {
   int fsr1 = analogRead(FSR_1);
   int fsr2 = analogRead(FSR_2);
   int fsr3 = analogRead(FSR_3);
+  int fsr4 = analogRead(FSR_4);
 
   Serial.print(fsr0);
   Serial.print(",");
+
   Serial.print(fsr1);
   Serial.print(",");
+
   Serial.print(fsr2);
   Serial.print(",");
-  Serial.println(fsr3);
 
-  delay(150); // 稍微稳定数据
+  Serial.print(fsr3);
+  Serial.print(",");
+
+  Serial.println(fsr4);
+
+  delay(80); 
 }
